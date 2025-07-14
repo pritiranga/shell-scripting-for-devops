8e1b39824a66d965452063e64336e53185d4df28
Ubuntu <ubuntu@ip-172-31-92-251.ec2.internal>
2025-07-14 07:07:08 +0000

    adding basic concepts

---
+++ 02_comments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Single line comments"
+#single line comment
+
+echo "Multi line comment"
+<<COMMENT
+This
+is
+multi
+line
+comment
+COMMENT
---
+++ 03_variables.sh
@@ -0,0 +1,12 @@
+#Script to show how to use variables
+
+#!/bin/bash
+
+ecode=323
+name="John"
+role="System Administrator"
+
+echo "Employee Id of $name is $ecode who is working as a $role."
+
+name="Paul"
+echo "Find the employee id of $name!"
