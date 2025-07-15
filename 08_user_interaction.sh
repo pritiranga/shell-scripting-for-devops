31b2b22f7703b9c3d865a005d7828de17de96fda
pritiranga <pritiranga9@gmail.com>
2025-07-15 09:29:29 +0000

    adding user interaction

--- 08_user_interaction.sh
+++ 08_user_interaction.sh
@@ -2,7 +2,10 @@
 
 #!/bin/bash
 
+# Ask for user input, but without any prompt message
 read role
+
+#Ask for user input but with proper message so user can understand what value is needed.
 read -p "What is your role? " designation
 
 echo "$designation"
